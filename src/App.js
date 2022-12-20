7672d35857e00047790700b0fffacf867695939f
facundo Mendez <facundo.mendez@live.com.ar>
2022-12-19 22:22:12 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Home from './components/home/Home';
-import React, {useEffect, useState} from 'react'
+import React, {useState} from 'react'
 import Context from "./components/context/Context"
 import WaveAudio from './components/assets/waveAudio/WaveAudio';
 
@@ -13,18 +13,6 @@ function App() {
     setAudioActive(!audioActive)
   }
 
-
-  useEffect(() => {
-    const cursor = document.querySelector('.cursor')
-
-    cursor.style.visibility = 'hidden'
-    
-    document.addEventListener('mousemove', (event) => {
-      cursor.setAttribute(`style`, `top: ${event.clientY - 6}px; left: ${event.clientX - 6}px`)
-    })
-
-    
-  },[])
 
   return (
     <Context.Provider value={{audioActive, setAudioActive , changeAudio }}>
