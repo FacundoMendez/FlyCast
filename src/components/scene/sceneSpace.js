95b2ec32efe3a939913e2ceb3114f36dba581cdd
facundo Mendez <facundo.mendez@live.com.ar>
2022-12-20 21:01:31 -0300

    prueba scene rendimiento

--- src/components/scene/sceneSpace.js
+++ src/components/scene/sceneSpace.js
@@ -1,6 +1,8 @@
 import * as THREE from 'three';
-import pisoTextura from "./src/textureFloor.png"
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
+import Mountains from "./src/Mountains.glb"
+import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
 
 const sceneSpace = () => {
 
@@ -45,6 +47,51 @@ const sceneSpace = () => {
         
         renderer.setSize(size.width, size.height)
         renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+       renderer.physicallyCorrectLights = true;
+ /*       renderer.outputEncoding = THREE.sRGBEncoding */
+
+        const controls = new OrbitControls(camera, canvas)
+        controls.enableDamping = true
+        controls.enablePan = false
+        controls.minDistance = 0
+        controls.maxDistance =1
+        controls.minPolarAngle = .8;
+        controls.maxPolarAngle = 2;
+
+
+
+        /* scene gltf */
+        let sceneMountains
+
+        const dracoLoader = new DRACOLoader()
+        dracoLoader.setDecoderPath('/draco/')
+
+        const gltfLoader = new GLTFLoader()
+        gltfLoader.setDRACOLoader(dracoLoader)
+        gltfLoader.load(
+            Mountains,
+            (gltf) =>
+            {
+                sceneMountains= gltf.scene
+                scene.add(sceneMountains)
+                sceneMountains.position.set(0,-1,0)
+                sceneMountains.scale.set(.3,.3,.3)
+            }
+        )
+
+        
+        const pointLight = new THREE.PointLight( 0xC776FF, 35 );
+        pointLight.position.set(0,0,0)
+        scene.add( pointLight );
+
+
+                
+        const pointLight2 = new THREE.PointLight( 0xC776FF, 35 );
+        pointLight2.position.set(0,0,-7)
+        scene.add( pointLight2 );
+
+     
+
 
        
       const animate = () =>{
@@ -52,6 +99,7 @@ const sceneSpace = () => {
           renderer.render(scene,camera)
           window.requestAnimationFrame(animate)
           renderer.autoClear = true
+          controls.update()
       }
       
       animate()
