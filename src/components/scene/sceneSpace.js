9a42e4564c06a407afe4b462d549af06a0610cc3
facundo Mendez <facundo.mendez@live.com.ar>
2022-12-23 22:31:54 -0300

    cambios aguia voladora

--- src/components/scene/sceneSpace.js
+++ src/components/scene/sceneSpace.js
@@ -1,11 +1,9 @@
 import * as THREE from 'three';
-/* import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'; */
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
-import Mountains from "./src/Mountains4.glb"
+import pajaro from "./src/vuelo.glb"
+
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'
-import gsap from 'gsap';
-import ScrollTrigger from 'gsap/ScrollTrigger';
-import { Elastic } from 'gsap';
 
 
 const sceneSpace = () => {
@@ -54,247 +52,119 @@ const sceneSpace = () => {
     renderer.setSize(size.width, size.height)
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
     renderer.physicallyCorrectLights = true;
- /*       renderer.outputEncoding = THREE.sRGBEncoding */
+   /*     renderer.outputEncoding = THREE.sRGBEncoding */
 
 
 
-    const cursor = {
-        x: 0,
-        y: 0
-    }
-    
-    window.addEventListener("mousemove", ( e ) => {
-        cursor.x =   e.x / size.width * 0.5
-        cursor.y = -(e.y / size.height * 0.5)
-    } )
+
     
 
+    const ambientLight = new THREE.AmbientLight(0xffffff ,5)
+    scene.add(ambientLight);
 
 
+    const floor = new THREE.PlaneGeometry(100, 100)
+    const materialFloor = new THREE.MeshBasicMaterial({color:"grey"})
+    const meshFloor = new THREE.Mesh(floor, materialFloor)
+    meshFloor.position.set(0, -3.5,0)
+    meshFloor.rotation.x = -Math.PI/2
+    scene.add(meshFloor)
+
+
+    const box1 = new THREE.BoxGeometry( 1 , 10 ,1)
+    const box1M = new THREE.MeshBasicMaterial({color:"red"})
+    const box1Mesh = new THREE.Mesh(box1, box1M)
+    box1Mesh.position.z = -50
+
+    scene.add(box1Mesh)
+
+
+    const box2 = new THREE.BoxGeometry( 1 , 10 ,1)
+    const box2M = new THREE.MeshBasicMaterial({color:"blue"})
+    const box2Mesh = new THREE.Mesh(box2, box2M)
+    box2Mesh.position.z = 50
+
+    scene.add(box2Mesh)
+
+
+    const box3 = new THREE.BoxGeometry( 1 , 10 ,1)
+    const box3M = new THREE.MeshBasicMaterial({color:"violet"})
+    const box3Mesh = new THREE.Mesh(box3, box3M)
+    box3Mesh.position.z = -0
+    box3Mesh.position.x = +50
+
+    scene.add(box3Mesh)
+
+
+    const box4 = new THREE.BoxGeometry( 1 , 10 ,1)
+    const box4M = new THREE.MeshBasicMaterial({color:"yellow"})
+    const box4Mesh = new THREE.Mesh(box4, box4M)
+    box4Mesh.position.z = -0
+    box4Mesh.position.x = -50
+
+    scene.add(box4Mesh)
+
+
+
+
+    
     /* scene gltf */
-    let sceneMountains
+
     let mixer = null
     const dracoLoader = new DRACOLoader()
     dracoLoader.setDecoderPath('/draco/')
 
     const gltfLoader = new GLTFLoader()
     gltfLoader.setDRACOLoader(dracoLoader)
+
+    let PajaroVuelo = null
+
     gltfLoader.load(
-        Mountains,
-        (gltf) =>
+        pajaro,
+        (modelVuelo) =>
         {
-            sceneMountains= gltf.scene
-            scene.add(sceneMountains)
-            sceneMountains.position.set(0,-.95,-5)
-            sceneMountains.scale.set(.3,.3,.3)
+            PajaroVuelo= modelVuelo.scene
+            scene.add(PajaroVuelo)
+            PajaroVuelo.position.set(0, -.4, -.5)
+            PajaroVuelo.scale.set(.3,.3,.3)
 
 
-            mixer = new THREE.AnimationMixer(gltf.scene)
-            const action = mixer.clipAction(gltf.animations[0])
-            action.setLoop(THREE.LoopRepeat);
+            mixer = new THREE.AnimationMixer(modelVuelo.scene)
+            const action = mixer.clipAction(modelVuelo.animations[1])
+            action.setEffectiveTimeScale(.5);
             action.play()
         }
     )
 
 
-    /* lights */
-
-
-
-        /* light top */
-    const pointLight = new THREE.PointLight( 0x9950FF, 48, 4 );
-    pointLight.position.set(0,-.58,0)
-    scene.add( pointLight );
-
-        /*light top-center  */
-    const pointLight4 = new THREE.PointLight( 0xA768FF, 40, 1.8);
-    pointLight4.position.set(1,-.4, -1.8)
-    scene.add( pointLight4 );
-         
-
-
-    /* light center */
-
-            /* light center button  */
-
-    const pointLight2 = new THREE.PointLight( 0x503477,0 ,1.5);
-    pointLight2.position.set(0,.2,-4.9)
-    scene.add( pointLight2 );
-
-            /* light center top  */
-
-    const pointLight6 = new THREE.PointLight( 0xA768FF,0 ,2.3);
-    pointLight6.position.set(0,-.5,-6)
-    scene.add( pointLight6 );
-
-
-    const pointLight5 = new THREE.PointLight( 0xA768FF,0 ,2.4);
-    pointLight5.position.set(1.5,1,-6)
-    scene.add( pointLight5 );
-
-
-    /* light bottom */
-
-    const pointLight3 = new THREE.PointLight( 0xAf76FF, 0 );
-    pointLight3.position.set(0,-.2,-11)
-    scene.add( pointLight3 );
-
-
-
-    /* animate */
-         
-    let setLight2 = false
-    let setLight3 = false
-
-
-    const movementScroll = () => {
-
-
-        
-        gsap.registerPlugin(ScrollTrigger);
-
-        const link_button1 = document.querySelector(".link_button1")
-        const link_button2 = document.querySelector(".link_button2")
-        const link_button3 = document.querySelector(".link_button3")
-
-
-        link_button1.addEventListener("click", () => {
-            gsap.to(camera.position , {
-                z:1,
-                duration: 1.2,
-                ease: Elastic
-
-            })
-            setLight2 = false
-            setLight3 = false
-
-            gsap.to(pointLight2 , {
-                intensity: 0  ,
-                duration: 1,
-                ease: Elastic
-            })
-            gsap.to(pointLight3 , {
-                intensity: 0  ,
-                duration: 1,
-                ease: Elastic
-            })
-
-            gsap.to(pointLight6 , {
-                intensity: 0  ,
-                duration: 1,
-                ease: Elastic
-            })
-
-            gsap.to(pointLight5 , {
-                intensity: 0  ,
-                duration: 1,
-                ease: Elastic
-            })
-        })
-
-
-
-        link_button2.addEventListener("click", () => {
-
-
-            const tl_button2 = gsap.timeline({
-                ease: Elastic,
-            })
-
-
-            tl_button2.to(camera.position , {
-                z: -4.1,
-                duration: 1.6,
-                ease: Elastic
-            })
-
-            if ( setLight2 === false){
-                tl_button2.to(pointLight6 , {
-                    intensity: 20  ,
-                    duration: .6,
-                    ease: Elastic
-                }).add(tl_button2.to(pointLight2 , {
-                    intensity: 40  ,
-                    duration: .6,
-                    ease: Elastic
-                }), "-=0.7")
-
-                tl_button2.to(pointLight5 , {
-                    intensity: 10 ,
-                    duration: 1,
-                    ease: Elastic
-                })
-        
-                setLight2 = true
-                setLight3 = false
-
-                gsap.to(pointLight3 , {
-                    intensity: 0  ,
-                    duration: 1,
-                    ease: Elastic
-                })
-
-     
-            }else{
-                tl_button2.to(pointLight2 , {
-                    intensity: 0  ,
-                    duration: 1,
-                    ease: Elastic
-                })
-            }
-
-    
-        })
-
-        link_button3.addEventListener("click", () => {
-
-            const tl_button3 = gsap.timeline({
-                ease: Elastic,
-            })
-
-            tl_button3.to(camera.position , {
-                z: -8,
-                duration: 1.2,
-                ease: Elastic
-            })
-
-            if ( setLight3 === false){
-                tl_button3.to(pointLight3 , {
-                    intensity: 40  ,
-                    duration: 1,
-                    ease: Elastic
-                })
-                setLight3 = true
-                setLight2 = false
-
-                gsap.to(pointLight2 , {
-                    intensity: 0  ,
-                    duration: 1,
-                    ease: Elastic
-                })
-                gsap.to(pointLight6 , {
-                    intensity: 0  ,
-                    duration: 1,
-                    ease: Elastic
-                })
-
-                gsap.to(pointLight5, {
-                    intensity: 0  ,
-                    duration: 1,
-                    ease: Elastic
-                })
-            }else{
-                tl_button3.to(pointLight3 , {
-                    intensity: 0  ,
-                    duration: 1,
-                    ease: Elastic
-                })
-            }
-    
-        })
+ 
+    const cursor = {
+        x: 0,
+        y: 0
     }
+
+    window.addEventListener("mousemove", ( e ) => {
+
+        cursor.x = (e.x / size.width * 2- 1) ;
+        cursor.y = -(e.y / size.height * 2 -1);
+    } )
+
+       
     
-    movementScroll()
+  
+    // Función para actualizar la posición y rotación de la cámara
+    const updateCamera = () => {
+
+        // Calcular la posición por encima del aguila
+        const targetPosition = PajaroVuelo.position.clone();
+        targetPosition.y += 1; // Añadir una unidad a la altura del aguila
+
+        camera.lookAt(PajaroVuelo.position)
+
+        // Mover la cámara suavemente hacia la posición del aguila
+        camera.position.lerp(targetPosition, 0.05);
+
+    }
 
 
 
@@ -302,28 +172,37 @@ const sceneSpace = () => {
 
     const animate = () =>{
 
-        const time = clock.getElapsedTime()
-        const ghost1Angle = time 
-
-        renderer.render(scene,camera)
-        window.requestAnimationFrame(animate)
+        const time =clock.getDelta();
         renderer.autoClear = true
-        /* controls.update() */
 
-        camera.position.y = cursor.y * .3
-        camera.position.x = cursor.x * .3
+        if (PajaroVuelo !== null) {
+          
+            const rotationY = (cursor.x * 1) % 180;
 
-        pointLight.position.x = Math.cos(ghost1Angle ) -3
-        pointLight2.position.x = -Math.sin(ghost1Angle ) *.5
+            // Calcula el ángulo de rotación en el eje X en función de la posición del cursor
+            // El límite de rotación es de 720 grados (2 vueltas completas)
+            const rotationX = (cursor.y * 1) % 180;
+            
+            // Rotación del modelo en el eje Y en función del ángulo calculado
+            PajaroVuelo.rotation.y = -rotationY * 4;
+            
+            // Rotación del modelo en el eje X en función del ángulo calculado
+            PajaroVuelo.rotation.x = rotationX;
 
-        pointLight3.position.x = Math.sin(ghost1Angle ) 
-        pointLight4.position.x = (Math.sin(ghost1Angle ) + .5) * -.4
-        pointLight6.position.x = (Math.cos(ghost1Angle ) ) * -.2
+            PajaroVuelo.translateZ(-0.1);
+
+            updateCamera()
+        }
+
 
         if(mixer)
         {
-            mixer.update(clock.getDelta() *6)
+            mixer.update(time)
         }
+  
+        
+        renderer.render(scene,camera)
+        window.requestAnimationFrame(animate)
     }
     
     animate()
